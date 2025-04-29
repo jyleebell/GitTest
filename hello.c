e1ef257d7a873747ad42a1cfd46fedbb0c6b2f7a
Bell Lee <jong@jbnu.ac.kr>
2025-04-29 13:39:05 +0900

    sum_it function is added.

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int
+sum_it (int a, int b) {
+  return a + b;
+}
+
+int
 main() {
-  printf("Hello, World!\n");
+  int i, sum = 0;
+
+  for (i = 0; i < 100; i++) {
+	sum = sum_it(i, sum);
+  }
+  printf("Hello, World! = %d\n", sum);
   return 0;
 }
